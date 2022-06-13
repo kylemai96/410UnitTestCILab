3f5c275607582ae332548efba3678bd2c6618cfc
kylemai96 <kylemai96@gmail.com>
2022-06-13 13:01:17 -0700

    first commit

---
+++ artifact/src/main/java/artifact/CILab.java
@@ -0,0 +1,20 @@
+package artifact;
+
+public class CILab implements CILabInterface {
+
+	public String getString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setString(String string) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean detectCapitalUse() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ artifact/src/test/java/artifact/CILabTest.java
@@ -0,0 +1,14 @@
+package artifact;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CILabTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
